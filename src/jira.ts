9465e4c4aa7efb2519cf4aa21ca9be5ed684c29d
AleDore <alessio85.dore@gmail.com>
2021-07-16 10:52:08 +0200

    small refactor

--- src/jira.ts
+++ src/jira.ts
@@ -15,12 +15,15 @@ export const JiraIssueResponse = t.exact(t.interface({
     fields: t.interface({
         key: t.string,
         summary: t.string,
-        issuetype: t.union([t.interface({
-            name: t.union([t.literal("Story"), t.literal("Epic"), t.literal("Bug"), t.literal("Task")])
-        }), t.interface({
-            name: t.string,
-            subtask: t.literal(true)
-        })]),
+        issuetype: t.union([
+            t.interface({
+                name: JiraIssueTypeName
+            }), 
+            t.interface({
+                name: t.string,
+                subtask: t.literal(true)
+            })
+        ]),
         project: t.interface({
             name: t.string
         })    
